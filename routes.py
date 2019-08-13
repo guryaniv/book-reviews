7e7b64b6238eab24de3441401bec6fbe25ac7fc9
guryaniv <guryaniv@gmail.com>
2019-08-13 18:56:03 +0300

    updated structure and todo, started api work and book model

--- routes.py
+++ routes.py
@@ -2,7 +2,7 @@ from . import app
 from .forms import LoginForm, SignupForm
 from flask import render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user
-from .models import User, add_user
+from .models import *
 from .config import basedir
 import os
 
@@ -59,10 +59,54 @@ def signup():
         print(form.errors)
         return render_template('signup.html', title='Sign Up', form=form)
 
+@app.route('/search')
+def search_page():
+    """search for a book. Users should be able to type in the ISBN number of a book, the title of a book,
+    or the author of a book. After performing the search, your website should display a list of possible
+     matching results, or some sort of message if there were no matches. If the user typed in only part of a title,
+      ISBN, or author name, your search page should find matches for those as well!"""
+    # TODO
 
+@app.route("/books/<int:book_id>")
+def book_page():
+    """When users click on a book from the results of the search page, they should be taken to a book page,
+     with details about the book: its title, author, publication year, ISBN number, and any reviews that users
+      have left for the book on your website."""
+    # TODO
+    """Review Submission: On the book page, users should be able to submit a review: consisting of a rating on
+     a scale of 1 to 5, as well as a text component to the review where the user can write their opinion about a book.
+      Users should not be able to submit multiple reviews for the same book."""
 
-@app.route('/testdb')
-def testdb():
+@app.route("/api/<int:isbn>")
+def api_access():
+    """If users make a GET request to your website’s /api/<isbn> route, where <isbn> is an ISBN number, your website should
+     return a JSON response containing the book’s title, author, publication date, ISBN number, review count, and average
+      score. The resulting JSON should follow the format:"""
+    # TODO
+    {
+        "title": "Memory",
+        "author": "Doug Lloyd",
+        "year": 2015,
+        "isbn": "1632168146",
+        "review_count": 28,
+        "average_score": 5.0
+    }
+
+@app.route('/testu')
+def testusers():
     users = User.query.all()
     print(users)
     return render_template("test.html", list=users, title="test")
+
+@app.route('/testb')
+def testbooks():
+    books = Book.query.all()
+    print(books)
+    return render_template("test.html", list=books, title="test")
+
+@app.route('/testapi')
+def testapi():
+    json = get_gr_reviews_data("0380795272")
+    book = json["books"]
+    return render_template("test.html", list=book, title="test")
+
